11f2a7f689f76074aaab66bf306a8e79585e9a56
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2024-09-05 11:08:19 +0200

    Add project files.

---
+++ ST10396397_PROG6212_POE_P1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ST10396397_PROG6212_POE_P1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
